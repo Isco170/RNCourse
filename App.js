4960b6d45f8172dcd375f699a3f9d8a2f85611df
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2023-01-08 17:43:26 +0200

    List

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button, 
+  TextInput, 
+  ScrollView, 
+  FlatList
+ } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText){
+    setEnteredGoalText(enteredText)
+  };
+
+  function addGoalHandler(){
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals, 
+      {text: enteredGoalText, id: Math.random().toString()},
+    ])
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' />
-        <Button title='Add goal' />
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler} />
+        <Button title='Add goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <Text >List of goals...</Text>
+        <FlatList 
+          data={courseGoals} 
+          renderItem={itemData => {
+            return(
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+        }}
+        keyExtractor={(item, index) =>{
+          return item.id;
+        }} 
+        alwaysBounceVertical={false} />
       </View>
     </View>
   );
@@ -38,5 +73,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer:{
     flex: 5
+  },
+  goalItem:{
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc'
+  },
+  goalText:{
+    color: 'white'
   }
 });
