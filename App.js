9fd1863a5d7f2a914aad66ce8d980f83d934ef19
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2023-05-23 20:38:35 +0200

    Handling Taps with the Pressable Component

--- App.js
+++ App.js
@@ -15,13 +15,17 @@ export default function App() {
     setCourseGoals(currentCourseGoal => [...currentCourseGoal, { text: enteredGoalText, id: Math.random().toString() }])
   };
 
+  function deleteGoalHandler(){
+    console.log('DELETE')
+  }
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler}/>
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals}
           renderItem={itemData => {
-            return <GoalItem text={itemData.item.text}/>;
+            return <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler}/>;
           }}
           keyExtractor={(item, index) => {
             return item.id
